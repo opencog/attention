fee44008262a153aa6f93b8d15a3479e630c1ab0
Linas Vepstas <linasvepstas@gmail.com>
2019-09-04 12:07:42 -0500

    Make a local copy of needed utilities

---
+++ opencog/attention/Neighbors.cc
@@ -0,0 +1,81 @@
+/*
+ * Neighbors.cc
+ *
+ * Copyright (C) 2014 OpenCog Foundation
+ *
+ * Author: William Ma <https://github.com/williampma>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <iostream>
+
+#include <opencog/atoms/base/Link.h>
+#include <opencog/atoms/base/Node.h>
+#include "Neighbors.h"
+
+namespace opencog
+{
+
+HandleSeq get_target_neighbors(const Handle& h, Type desiredLinkType,
+                               bool match_subtype/* = false*/)
+{
+    if (nameserver().isA(desiredLinkType, UNORDERED_LINK))
+        return HandleSeq();
+
+    HandleSeq answer;
+    for (const LinkPtr& link : h->getIncomingSet())
+    {
+        Type t = link->get_type();
+        if (not(t == desiredLinkType or
+               (match_subtype and nameserver().isA(t, desiredLinkType))))
+	    continue;
+        if (link->getOutgoingAtom(0) != h) continue;
+
+        for (const Handle& handle : link->getOutgoingSet()) {
+           if (handle == h) continue;
+           answer.emplace_back(handle);
+        }
+    }
+    return answer;
+}
+
+
+HandleSeq get_source_neighbors(const Handle& h, Type desiredLinkType,
+                               bool match_subtype/* = false*/)
+{
+    if (nameserver().isA(desiredLinkType, UNORDERED_LINK))
+        return HandleSeq();
+
+    HandleSeq answer;
+
+    for (const LinkPtr& link : h->getIncomingSet())
+    {
+        Type t = link->get_type();
+        if (not(t == desiredLinkType or
+               (match_subtype and nameserver().isA(t, desiredLinkType))))
+	    continue;
+        if (link->getOutgoingAtom(0) == h) continue;
+
+        for (const Handle& handle : link->getOutgoingSet()) {
+           if (handle == h) continue;
+            answer.emplace_back(handle);
+        }
+    }
+    return answer;
+}
+
+} // namespace OpenCog
